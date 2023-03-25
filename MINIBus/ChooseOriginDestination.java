da28e1b7c0991331a155d7adcaca39a9e5c63158
natcharapaphon301045 <6431503019@lamduan.mfu.ac.th>
2023-03-26 00:30:42 +0700

    update Chooseoriginal wait for keyboard reading

--- MINIBus/ChooseOriginDestination.java
+++ MINIBus/ChooseOriginDestination.java
@@ -3,4 +3,32 @@ package MINIBus;
 /*---------------------------------------------1. Choose the origin and destination------------------------------------------------------------------------*/
 public class ChooseOriginDestination {
     
+    public boolean ChooseOrigin(String oString) {
+        String[] stations = StationArray();
+        
+        System.out.println("Select original stutation");
+        for (String station : stations) {
+            System.out.println(station);
+            /*ADD keyboard reading */
+           
+        }
+        return true;
+    }
+    /*------------------------------------------Array--------------------------------------------------------------------------------------------------------*/
+    public String[]StationArray() {
+        String[] stations = new String[10];
+        stations[0] = "station 1 -??? Baht";
+        stations[1] = "station 2 -??? Baht";
+        stations[2] = "station 3 -??? Baht";
+        stations[3] = "station 4 -??? Baht";
+        stations[4] = "station 5 -??? Baht";
+        stations[5] = "station 6 -??? Baht";
+        stations[6] = "station 7 -??? Baht";
+        stations[7] = "station 8 -??? Baht";
+        stations[8] = "station 9 -??? Baht";
+        stations[9] = "station 10-??? Baht";
+        return stations;
+    }
+    /*------------------------------------------Array--------------------------------------------------------------------------------------------------------*/
 }
+
--- MINIBus/Main.java
+++ MINIBus/Main.java
@@ -1,2 +1,16 @@
 package MINIBus;
 
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Create an instance of the ChooseOriginDestination class
+        ChooseOriginDestination originDestination = new ChooseOriginDestination();
+
+        // Call the ChooseOrigin method with a sample string argument
+        boolean result = originDestination.ChooseOrigin("sample string");
+
+        
+    }
+}
