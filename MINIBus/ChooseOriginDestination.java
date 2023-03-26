326814b7a8e70991827547e0caaafb7d09b13066
natcharapaphon301045 <6431503019@lamduan.mfu.ac.th>
2023-03-26 23:53:09 +0700

    update main,ChooseOriginDestination

--- MINIBus/ChooseOriginDestination.java
+++ MINIBus/ChooseOriginDestination.java
@@ -1,32 +1,75 @@
 package MINIBus;
+import java.util.Scanner;
 
 /*---------------------------------------------1. Choose the origin and destination------------------------------------------------------------------------*/
 public class ChooseOriginDestination {
-    
+    /*------------------------------------------1.1 Choose the original------------------------------------------------------------------------------------*/
     public boolean ChooseOrigin(String oString) {
         String[] stations = StationArray();
         
-        System.out.println("Select original stutation");
-        for (String station : stations) {
-            System.out.println(station);
-            /*ADD keyboard reading */
-           
+        System.out.println("the riginal stutation");
+        /*--------------------------------------Print array station information------------------------------------------------------*/
+        for (int i = 0; i < stations.length; i++) {
+            System.out.println((i+1) + ". " + stations[i]);
         }
+        System.out.println("Select original stutation");
+        /*--------------------------------------ADD keyboard reading-----------------------------------------------------------------*/
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        try {
+            int selection = Integer.parseInt(input);
+            if (selection > 0 && selection <= stations.length) {
+                System.out.println("You selected: " + stations[selection-1]);
+            } else {
+                System.out.println("Invalid selection.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+        }
+        
+        return true;
+    }
+    /*------------------------------------------1.2 Choose the Destination----------------------------------------------------------------------------------*/
+    public boolean ChooseDestination(String oString){
+        String[] stations = StationArray();
+
+        System.out.println("the Destination stutation");
+        /*--------------------------------------Print array station information------------------------------------------------------*/
+        for (int i = 0; i < stations.length; i++) {
+            System.out.println((i+1) + ". " + stations[i]);
+        }
+        System.out.println("Select destination stutation");
+        /*--------------------------------------ADD keyboard reading-----------------------------------------------------------------*/
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        try {
+            int selection = Integer.parseInt(input);
+            if (selection > 0 && selection <= stations.length) {
+                System.out.println("You selected: " + stations[selection-1]);
+            } else {
+                System.out.println("Invalid selection.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+        }
+        
         return true;
     }
     /*------------------------------------------Array--------------------------------------------------------------------------------------------------------*/
     public String[]StationArray() {
         String[] stations = new String[10];
-        stations[0] = "station 1 -??? Baht";
-        stations[1] = "station 2 -??? Baht";
-        stations[2] = "station 3 -??? Baht";
-        stations[3] = "station 4 -??? Baht";
-        stations[4] = "station 5 -??? Baht";
-        stations[5] = "station 6 -??? Baht";
-        stations[6] = "station 7 -??? Baht";
-        stations[7] = "station 8 -??? Baht";
-        stations[8] = "station 9 -??? Baht";
-        stations[9] = "station 10-??? Baht";
+        stations[0] = "station 1 "+Integer.toString(100)+" Baht";
+        stations[1] = "station 2 "+Integer.toString(200)+" Baht";
+        stations[2] = "station 3 "+Integer.toString(300)+" Baht";
+        stations[3] = "station 4 "+Integer.toString(400)+" Baht";
+        stations[4] = "station 5 "+Integer.toString(500)+" Baht";
+        stations[5] = "station 6 "+Integer.toString(600)+" Baht";
+        stations[6] = "station 7 "+Integer.toString(700)+" Baht";
+        stations[7] = "station 8 "+Integer.toString(800)+" Baht";
+        stations[8] = "station 9 "+Integer.toString(900)+" Baht";
+        stations[9] = "station 10 "+Integer.toString(1000)+" Baht";
         return stations;
     }
     /*------------------------------------------Array--------------------------------------------------------------------------------------------------------*/
--- MINIBus/Main.java
+++ MINIBus/Main.java
@@ -3,14 +3,20 @@ package MINIBus;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("MINIbus program");
 
         // Create an instance of the ChooseOriginDestination class
         ChooseOriginDestination originDestination = new ChooseOriginDestination();
 
         // Call the ChooseOrigin method with a sample string argument
-        boolean result = originDestination.ChooseOrigin("sample string");
-
+        boolean originResult = originDestination.ChooseOrigin("sample origin string");
         
+        // Call the ChooseDestination method with a sample string argument
+        boolean destinationResult = originDestination.ChooseDestination("sample destination string");
+        
+        //Print the results to the console Check status
+        /*System.out.println("Your origin station: "+originResult);
+        System.out.println("Your destination station: "+destinationResult);
+        */
     }
 }
