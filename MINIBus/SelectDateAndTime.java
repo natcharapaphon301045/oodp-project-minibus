049acc0920c239676a13bd6149118ab1c9216c1b
natcharapaphon301045 <6431503019@lamduan.mfu.ac.th>
2023-03-27 00:05:52 +0700

    electDateAndTime little update

--- MINIBus/SelectDateAndTime.java
+++ MINIBus/SelectDateAndTime.java
@@ -1,5 +1,7 @@
 package MINIBus;
-
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
 /*-----------------------------------------2.1 Select departure date ((Reserve up to 7 days in advance)-------------------------------------------------------*/
 /*-----------------------------------------2.2 Choose the desired time slot-----------------------------------------------------------------------------------*/
 
