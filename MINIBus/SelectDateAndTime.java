fc2a3c5b79931894914d6a1ac1e129b4ba8b7d5e
Buntita Atthahan <6531503043@lamduan.mfu.ac.th>
2023-04-29 06:14:16 +0000

    เปลี่ยนDateformat

--- MINIBus/SelectDateAndTime.java
+++ MINIBus/SelectDateAndTime.java
@@ -4,20 +4,22 @@
 //TRY (T_T) P'Jab check hai nong duay naka 
 public class SelectDateAndTime {
     
-	public static void SelectDate(String[] args) {
-	   LocalDateTime today = LocalDateTime.now();
+	public static void SelectDate(String[] args) {	
+		LocalDate today = LocalDate.now();
 	   System.out.print("Today is " +today);
+	
+	   LocalDate FirstDate = today.plusDays(1);
+	   LocalDate LastDate = today.plusDays(7);
+	   System.out.println("You can select the date between" + FirstDate + "and" + LastDate);
    
-	   LocalDateTime FirstDate = now.plusDays(1);
-	   LocalDateTime LastDate = now.plusDays(7);
-	   System.out.print("You can select the date between" + FirstDate + "and" + LastDate);
-	   
+	   DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	
 	   Scanner scanner = new Scanner(System.in);
-	   System.out.print("Enter departure date");
+	   System.out.print("Enter departure date (yyyy-MM-dd)");
 	   String inputDate = scanner.nextLine();
-	   DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-	   LocalDateTime departureDate = LocalDateTime.parse(inputDate, formatter);
-
+	  
+	   LocalDate departureDate = LocalDate.parse(inputDate, formatter);
+	  
 	   if (departureDate.isBefore(FirstDate) || departureDate.isAfter(LastDate)) {
 			 System.out.println("Departure date must be between " + FirstDate.format(formatter) 
 			 + " and " + LastDate.format(formatter) + ".");
