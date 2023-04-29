388947f4d570054cb1a447dd08212e19b1aa04ba
Buntita Atthahan <6531503043@lamduan.mfu.ac.th>
2023-04-29 21:43:00 +0000

    add time

--- MINIBus/SelectDateAndTime.java
+++ MINIBus/SelectDateAndTime.java
@@ -31,3 +31,61 @@ public class SelectDateAndTime {
 	   scanner.close();
 	}
 }
+/*-----------------------------------------2.2 Choose the desired time slot-----------------------------------------------------------------------------------*/
+
+public static void main(String[] args) {
+	selectTimeSlot();
+}
+    private static final String TIME_PATTERN = "H:mm";
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_PATTERN);
+
+    private static final LocalTime[] TimeSlot = {
+            LocalTime.parse("5:30", TIME_FORMATTER),
+            LocalTime.parse("7:00", TIME_FORMATTER),
+            LocalTime.parse("8:30", TIME_FORMATTER),
+            LocalTime.parse("10:00", TIME_FORMATTER),
+            LocalTime.parse("11:30", TIME_FORMATTER),
+            LocalTime.parse("13:00", TIME_FORMATTER),
+            LocalTime.parse("14:30", TIME_FORMATTER),
+            LocalTime.parse("16:00", TIME_FORMATTER),
+            LocalTime.parse("17:30", TIME_FORMATTER),
+            LocalTime.parse("19:00", TIME_FORMATTER)
+    };
+
+    public static void selectTimeSlot() {
+        LocalDateTime currentTime = LocalDateTime.now();
+		
+  /*--------------------------------------Print------------------------------------------------------*/	
+		System.out.println("Available time slots:");
+		for (int i = 0; i < TimeSlot.length; i++) {
+            String formattedTime = TimeSlot[i].format(TIME_FORMATTER);
+            System.out.println((i + 1) + ". " + formattedTime);
+        }
+  /*------------------------------------------------------------------------------------------------*/
+        for (int i = 0; i < TimeSlot.length; i++) {
+            LocalTime timeSlot = TimeSlot[i];
+            LocalDateTime startTime = timeSlot.atDate(currentTime.toLocalDate());
+
+            if (startTime.isAfter(currentTime)) {
+                String formattedTime = timeSlot.format(TIME_FORMATTER);
+                System.out.println((i + 1) + ". " + formattedTime);
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int selectedTimeSlotNumber = -1;
+
+        do {
+            System.out.print("Enter the number of the time slot you would like to select: ");
+            if (scanner.hasNextInt()) {
+                selectedTimeSlotNumber = scanner.nextInt();
+            } else {
+                scanner.nextLine(); 
+            }
+        } while (selectedTimeSlotNumber < 1 || selectedTimeSlotNumber > TimeSlot.length);
+
+        String selectedTimeSlot = TimeSlot[selectedTimeSlotNumber - 1].format(TIME_FORMATTER);
+        System.out.println("You have selected the time slot: " + selectedTimeSlot);
+    }
+
+//เลือกเวลาได้แต่บัค เลือกเวลาที่ผ่านมาแล้วได้ TT
