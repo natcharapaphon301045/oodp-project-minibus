3fa582b95a3118365eadd6caf13596c38cbf2bdd
natcharapaphon301045 <6431503019@lamduan.mfu.ac.th>
2023-05-01 11:30:22 +0700

    minifix bug

--- MINIBus/SelectDateAndTime.java
+++ MINIBus/SelectDateAndTime.java
@@ -1,5 +1,8 @@
-/*-----------------------------------------2.1 Select departure date ((Reserve up to 7 days in advance)-------------------------------------------------------*/
-/*-----------------------------------------2.2 Choose the desired time slot-----------------------------------------------------------------------------------*/
+package MINIBus;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
 
 //TRY (T_T) P'Jab check hai nong duay naka 
 public class SelectDateAndTime {
